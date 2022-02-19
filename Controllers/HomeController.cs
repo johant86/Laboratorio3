9a17ffd192efac167099f47b151313eb35c413ba
jvivas <jvivas@JOHANNY5564>
2022-02-19 12:18:20 -0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Laboratorio3.Models;
+using LibraryData;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Laboratorio3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ISentenceService _sentence;
+
+        public HomeController(ISentenceService sentence)
+        {
+            _sentence = sentence;
+        }
+
+        public IActionResult Index()
+        {
+            Sentence sentence = new Sentence();
+            string one =  _sentence.GeChartArt("Visual Studio, 2022.", 0);
+            sentence.chain = _sentence.ConcatLetters(one);
+
+            string word = _sentence.WebScraping();
+            string two = _sentence.FindLeader(word, "i");
+            sentence.chain = _sentence.ConcatLetters(two);
+
+            string three = _sentence.IndexOfData("Visual Studio, 2022.", "s");
+            sentence.chain = _sentence.ConcatLetters(three);
+
+            return View(sentence);
+        }
+
+    }
+}
---
+++ Models/Sentence.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laboratorio3.Models
+{
+    public class Sentence
+    {
+        public string chain { set; get; }
+    }
+}
